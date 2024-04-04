b8e6cb29792d3cc2e0515d57909bd49a3f8da719
sameedbaqai94 <sameedbaqai94@gmail.com>
2024-04-04 02:04:51 -0400

    Added addList, Promise to clear out domcontentloading

--- dist/index.js
+++ dist/index.js
@@ -47,24 +47,66 @@ function Main() {
             }
         });
     }
+    function _addItem(itemObj) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(itemObj)
+            };
+            try {
+                const response = yield fetch("http://localhost:8080/api/Items/AddItem?todolistid=1", requestOptions);
+                if (!response.ok) {
+                    return false;
+                }
+                return true;
+            }
+            catch (error) {
+                return false;
+            }
+        });
+    }
     //Public Functions
     //controllers
     this.getList = function () {
         return __awaiter(this, void 0, void 0, function* () {
             const data = yield _getList();
-            const listContainer = document.getElementById("listsContainer");
+            const listTemplate = document.getElementById("listTemplate");
             //listContainer.textContent = data.title;
             data.forEach(element => {
-                const h2 = document.createElement("h2");
-                h2.textContent = element.title;
-                listContainer.appendChild(h2);
+                if (element.title != "") {
+                    const list = document.createElement('div');
+                    list.setAttribute("class", "list");
+                    const h2 = document.createElement("h2");
+                    h2.setAttribute("class", "list-title");
+                    const ul = document.createElement("ul");
+                    ul.setAttribute("class", "items");
+                    const form = document.createElement("form");
+                    form.setAttribute("class", "itemForm");
+                    const input = document.createElement("input");
+                    input.type = "text";
+                    input.name = "description";
+                    input.setAttribute("class", "itemInput");
+                    input.placeholder = "Add new item...";
+                    const button = document.createElement("button");
+                    button.type = "submit";
+                    button.textContent = "Submit";
+                    h2.textContent = element.title;
+                    form.appendChild(input);
+                    form.appendChild(button);
+                    list.appendChild(h2);
+                    list.appendChild(ul);
+                    list.appendChild(form);
+                    listTemplate.appendChild(list);
+                }
             });
         });
     };
     this.addList = function (formData) {
         const formObj = {
-            title: "",
-            items: []
+            title: ""
         };
         formData.forEach((val) => { formObj.title = val.toString(); });
         console.log(formObj);
@@ -72,16 +114,60 @@ function Main() {
             console.log("Something went wrong");
         }
         console.log("List added");
-        window.location.reload();
+        //window.location.reload();
+    };
+    this.addItem = function (itemFormData) {
+        const itemObj = {
+            description: "",
+            status: ""
+        };
+        itemFormData.forEach((element) => {
+            itemObj.description = element.toString();
+            itemObj.status = "inactive";
+        });
+        console.log(itemObj);
+        if (!_addItem(itemObj)) {
+            console.log("Something went wrong");
+        }
+        console.log("Item added");
     };
 }
-document.addEventListener("DOMContentLoaded", function () {
-    let App = new Main();
-    const form = document.getElementById("listForm");
+function domContentLoadedPromise() {
+    return new Promise((resolve) => {
+        if (document.readyState == "complete") {
+            resolve();
+        }
+        else {
+            document.addEventListener('DOMContentLoaded', () => resolve());
+        }
+    });
+}
+function dynamicLoadedPromise(selector) {
+    return new Promise((resolve) => {
+        const eventListener = (event) => {
+            event.preventDefault();
+            const target = event.target;
+            if (target.matches(selector)) {
+                resolve(target);
+            }
+        };
+        document.addEventListener('submit', eventListener);
+    });
+}
+const App = new Main();
+domContentLoadedPromise().then(() => {
+    const listForm = document.getElementById("listForm");
     App.getList();
-    form.addEventListener("submit", function (event) {
+    listForm.addEventListener("submit", function (event) {
         event.preventDefault();
-        const formData = new FormData(form);
+        const formData = new FormData(listForm);
+        console.log(formData);
         App.addList(formData);
     });
+    return dynamicLoadedPromise(".itemForm");
+}).then((clickedElement) => {
+    console.log('Clicked element:', clickedElement);
+    const itemData = new FormData(clickedElement);
+    console.log(itemData);
+    App.addItem(itemData);
 });
--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,16 @@ interface Item {
 }
 interface RequestOption {
     method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-    headers: Record<string, string>,
-    body: string
+    headers?: Record<string, string>,
+    body?: string
 }
 interface IMain {
     _getList: () => Promise<string[]>
     _addList: (formObj: ToDoList) => boolean;
+    _addItem: (itemObj: Item) => boolean;
     getList: () => void;
     addList: (formData: FormData) => void;
+    addItem: (itemFormData: FormData) => void;
 }
 
 function Main(this: IMain) {
@@ -51,23 +53,65 @@ function Main(this: IMain) {
             return false;
         }
     }
+    async function _addItem(itemObj: Item) {
+        const requestOptions: RequestOption = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(itemObj)
+        }
+        try {
+            const response = await fetch("http://localhost:8080/api/Items/AddItem?todolistid=1", requestOptions);
+            if (!response.ok) {
+                return false;
+            }
+            return true
+        } catch (error) {
+            return false;
+        }
+    }
+
     //Public Functions
     //controllers
     this.getList = async function () {
         const data: ToDoList[] = await _getList();
-        const listContainer = document.getElementById("listsContainer") as HTMLDivElement;
+        const listTemplate = document.getElementById("listTemplate") as HTMLDivElement;
         //listContainer.textContent = data.title;
         data.forEach(element => {
-            const h2 = document.createElement("h2") as HTMLHeadElement;
-            h2.textContent = element.title;
-            listContainer.appendChild(h2);
+            if (element.title != "") {
+                const list = document.createElement('div') as HTMLDivElement;
+                list.setAttribute("class", "list");
+                const h2 = document.createElement("h2") as HTMLHeadElement;
+                h2.setAttribute("class", "list-title");
+                const ul = document.createElement("ul") as HTMLUListElement;
+                ul.setAttribute("class", "items");
+                const form = document.createElement("form") as HTMLFormElement;
+                form.setAttribute("class", "itemForm");
+                const input = document.createElement("input") as HTMLInputElement;
+                input.type = "text";
+                input.name = "description";
+                input.setAttribute("class", "itemInput");
+                input.placeholder = "Add new item...";
+                const button = document.createElement("button") as HTMLButtonElement;
+                button.type = "submit";
+                button.textContent = "Submit";
+
+                h2.textContent = element.title;
+                form.appendChild(input);
+                form.appendChild(button);
+                list.appendChild(h2);
+                list.appendChild(ul);
+                list.appendChild(form);
+                listTemplate.appendChild(list);
+            }
+
         });
     }
 
     this.addList = function (formData: FormData) {
         const formObj: ToDoList = {
-            title: "",
-            items: []
+            title: ""
         }
 
         formData.forEach((val) => { formObj.title = val.toString(); })
@@ -76,19 +120,67 @@ function Main(this: IMain) {
             console.log("Something went wrong");
         }
         console.log("List added");
-        window.location.reload();
+        //window.location.reload();
+    }
+    this.addItem = function (itemFormData: FormData) {
+        const itemObj: Item = {
+            description: "",
+            status: ""
+        }
+        itemFormData.forEach((element) => {
+            itemObj.description = element.toString();
+            itemObj.status = "inactive";
+        })
+        console.log(itemObj);
+        if (!_addItem(itemObj)) {
+            console.log("Something went wrong");
+        }
+        console.log("Item added");
     }
 }
-document.addEventListener("DOMContentLoaded", function () {
-    let App: IMain = new (Main as any)();
-    const form = document.getElementById("listForm") as HTMLFormElement;
+
+
+function domContentLoadedPromise() {
+    return new Promise<void>((resolve) => {
+        if (document.readyState == "complete") {
+            resolve();
+        } else {
+            document.addEventListener('DOMContentLoaded', () => resolve());
+        }
+    })
+}
+function dynamicLoadedPromise(selector: string): Promise<HTMLFormElement> {
+    return new Promise<HTMLFormElement>((resolve) => {
+        const eventListener = (event: Event) => {
+            event.preventDefault();
+            const target = event.target as HTMLFormElement;
+            if (target.matches(selector)) {
+                resolve(target);
+            }
+        };
+        document.addEventListener('submit', eventListener);
+    })
+}
+
+const App: IMain = new (Main as any)();
+domContentLoadedPromise().then(() => {
+
+    const listForm = document.getElementById("listForm") as HTMLFormElement;
+
     App.getList();
 
-    form.addEventListener("submit", function (event) {
+    listForm.addEventListener("submit", function (event) {
         event.preventDefault();
-        const formData = new FormData(form);
+        const formData = new FormData(listForm);
+        console.log(formData);
         App.addList(formData);
+    });
+    return dynamicLoadedPromise(".itemForm");
 
-    })
-});
+}).then((clickedElement) => {
+    console.log('Clicked element:', clickedElement);
+    const itemData = new FormData(clickedElement);
+    console.log(itemData);
+    App.addItem(itemData);
+})
 
